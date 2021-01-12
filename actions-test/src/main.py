f8355866ccb67fcee12d7a656d0c1212471e76f5
Jeremiah LaPierre <jlapierre@istreamplanet.com>
2021-01-12 11:00:59 -0600

    adding random number

--- actions-test/src/main.py
+++ actions-test/src/main.py
@@ -1,4 +1,8 @@
-import os
-#with open('./README.MD') as f:
-#    print(f.read())
-print(os.getcwd())
+
+from random import seed
+from random import randint
+
+seed(1)
+value = randint(0, 100)
+with open('/github/workspace/README.md', 'w') as f:
+    f.write(f"# {value}")
