8a258c4e7543d39815f8af24bb1e78d5c0a945a8
jlapierre0istream <46462039+jlapierre0istream@users.noreply.github.com>
2021-01-14 15:51:34 -0600

    Update main.py

--- actions-test/src/main.py
+++ actions-test/src/main.py
@@ -1,6 +1,14 @@
-
+import argparse
 import time
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--output_path", 
+                    default="CLUSTERINFO.md", 
+                    type=str)
+args = parser.parse_args()
+
+OUTPUT_PATH = args.output_path
 
 now = int(time.time())
 with open('README.md', 'w') as f:
-    f.write(f"# {now}")
+    f.write(f"# {now} {OUTPUTPATH}")
